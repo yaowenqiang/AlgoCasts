eda57deaf82c77a41d324e0d6b7ebb7e71bb5dac
yaowenqiang <yaowenqiang111@163.com>
2021-02-27 23:45:02 +0800

    finish rest version

--- exercises/reversestring/main.rs
+++ exercises/reversestring/main.rs
@@ -1,14 +1,25 @@
 fn main() {
-
-    println!("hello world");
-    println!("{}", reverse("abc你好"))
+    let s = String::from("hello world");
+    println!("{}", reverse(s));
+    let s = String::from("你好");
+    println!("{}", reverse(s));
 }
 
+/*
 fn reverse(s :  &str) -> &'static str {
-    let mut retunstr = String.from;
+    let mut retunstr = String.from(s);
     for i in s.chars() {
 	retunstr = format!("{}{}",i, retunstr);
     }
     println!("{}", &retunstr);
     return  ""
+}
+*/
+fn reverse(mut s: String) -> String {
+    let mut ss = String::from("");
+    while s.len() > 0 {
+        //let c = s.pop().unwrap();
+        ss.push(s.pop().unwrap());
+    }
+    return ss
 }
