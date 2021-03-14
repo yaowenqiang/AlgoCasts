4cc6c55c9e89f92bb0f1a1be112ee77615fd2881
yaowenqiang <yaowenqiang111@163.com>
2021-03-15 00:42:13 +0800

    add go steps v2

--- exercises/steps/main.go
+++ exercises/steps/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-    step(10)
+    //step(10)
+    fmt.Println(stepv2(10))
 }
 
 
@@ -15,6 +16,24 @@ func step(s int) {
       rp(" ", s -i)
       rp("\n", 1)
     }
+}
+
+
+// step v2, only just make the string, one print call
+
+func stepv2(s int) string {
+    var matrix []byte
+    for i := 0; i < s; i++ {
+        for j := 0; j < s; j++ {
+            if j <=i {
+                matrix = append(matrix,'#')
+            } else {
+               matrix = append(matrix, ' ')
+            }
+        }
+        matrix = append(matrix,'\n')
+    }
+    return string(matrix)
 }
 
 func rp(s string, count int) {
