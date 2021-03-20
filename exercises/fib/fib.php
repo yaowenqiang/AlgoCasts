804700ad2a876719fb8e99ffe16dd7174261b4fb
yaowenqiang <yaowenqiang111@163.com>
2021-03-20 23:55:34 +0800

    simpfy fastFib function

--- exercises/fib/fib.php
+++ exercises/fib/fib.php
@@ -32,25 +32,25 @@ function memo($n)
 function fastFib($n)
 {
     static $cache = [0, 1];
-    static $count = 0;
-    $count++;
-    var_dump($count);
-    static $self = __FUNCTION__;
-    if (isset($cache[$n])) {
-        return $cache[$n];
-    } else {
-        $result = $self($n-1) + $self($n - 2);
-        //$result = "{__FUNCTION__}"($n-1) + "{__FUNCTION__}"($n - 2);
-        $cache[$n] = $result;
-        return $result;
+    //static $self = __FUNCTION__;
+    //static $count = 0;
+    //$count++;
+    //var_dump($count);
+    /*
+    if (!isset($cache[$n])) {
+        //$cache[$n] = $self($n-1) + $self($n - 2);
+        $cache[$n] = fastFib($n-1) + fastFib($n - 2);
     }
+     */
+    isset($cache[$n])?:$cache[$n] = fastFib($n - 1) + fastFib($n - 2);
+    return $cache[$n];
 }
 
 function slowFib($n)
 {
     static $count = 0;
-    $count++;
-    var_dump($count);
+    //$count++;
+    //var_dump($count);
 
     if($n == 0 || $n == 1) {
         return $n;
@@ -59,6 +59,6 @@ function slowFib($n)
 }
 
 //var_dump('result:' . fib(12));
-var_dump('result:' . fastFib(10));
-var_dump('result:' . slowFib(10));
+var_dump('result:' . fastFib(30));
+var_dump('result:' . slowFib(30));
 ?>
