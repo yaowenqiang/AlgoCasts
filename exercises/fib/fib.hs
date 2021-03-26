6aafe188a578c2e12c505841883c7224e21aaee3
Yaowenqiang <yaowenqiang111@163.com>
2021-03-26 20:26:32 +0800

    add memoization fib

--- exercises/fib/fib.hs
+++ exercises/fib/fib.hs
@@ -1,1 +1,12 @@
-fib n =  if n <= 0 then 0 else if n == 1 then 1 else (fib n-1) + (fib n-2)
+--fib n =  if n <= 0 then 0 else if n == 1 then 1 else fib (n-2) + fib (n-1)
+fib 0 = 0
+fib 1 = 1
+--fib n = (fib n-1) + (fib n-2)
+fib n = fib (n-1) + fib (n-2)
+
+
+memoized_fib :: Int -> Integer
+memoized_fib = (map fib [0 ..] !!)
+   where fib 0 = 0
+         fib 1 = 1
+         fib n = memoized_fib (n-2) + memoized_fib (n-1)
