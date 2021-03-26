7a7970a0ea2b30d65d936253a438113344cf0b91
yaowenqiang <yaowenqiang111@163.com>
2021-03-27 02:44:07 +0800

    add Remove method of Node

--- exercises/tree/tree.go
+++ exercises/tree/tree.go
@@ -14,6 +14,34 @@ func(n *Node)Add(data int) {
     n.children = append(n.children, node)
 }
 
+/*
+func(n *Node)Remove(data int) {
+    for i :=0; i <  len(n.children); i ++ {
+        if n.children[i].data == data {
+            n.children = append(n.children[:i], n.children[i+1:]...)
+        }
+    }
+}
+
+*/
+
+func(n *Node)Remove(data int) {
+    for  {
+        hit := 0
+        length := len(n.children);
+        for i := 0; i < length; i ++ {
+            if n.children[i].data == data {
+                n.children = append(n.children[:i], n.children[i+1:]...)
+                hit = 1
+                break
+            }
+        }
+        if hit == 0 {
+            break
+        }
+    }
+}
+
 type Tree struct {
 
 }
@@ -28,6 +56,19 @@ func MakeNode(data int) *Node {
 func main() {
     n := MakeNode(1)
     n.Add(2)
+    n.Add(3)
+    n.Add(3)
+    n.Add(3)
+    n.Add(3)
+    n.Add(3)
+    n.Add(3)
+    n.Add(4)
+    fmt.Printf("%+v\n", n.children)
+    n.Remove(3)
+    n.Add(5)
+    n.Add(6)
+    n.Add(7)
+    n.Add(8)
     fmt.Printf("%+v\n", n.children)
 
 }
