0a05e1fdbace6da0e08864a5f385c4e80d029e25
yaowenqiang <yaowenqiang111@163.com>
2021-04-04 23:44:41 +0800

    add select sort method

---
+++ exercises/sorting/select_sort.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    arr := []int{1,2,3,4,5,6,6,5,4}
+    fmt.Println(selectSort(arr))
+}
+
+func selectSort (arr []int) []int {
+    for i := 0; i < len(arr); i++ {
+        minIndex := i
+        for j := i+1; j < len(arr); j++ {
+            if arr[j] < arr[minIndex] {
+                minIndex = j
+            }
+        }
+        if minIndex != i {
+            tmp := arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = tmp
+        }
+    }
+    return arr
+}
